8dc5b146d47a173b30bd783d5d54d5b33bb371ea
user.email <sawepeter6@gmail.com>
2018-05-20 16:45:59 +0300

    fixed a bug 🎨

--- app/src/main/java/com/example/sawepeter/eatout/SignUp.java
+++ app/src/main/java/com/example/sawepeter/eatout/SignUp.java
@@ -2,8 +2,14 @@ package com.example.sawepeter.eatout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignUp extends AppCompatActivity {
+
+    MaterialEditText edtphone,edtname,edtpassword;
+    Button btnsignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
