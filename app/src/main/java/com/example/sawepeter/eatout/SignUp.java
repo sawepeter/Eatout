fef688671e70a9518a434d5f77d74de34807b67b
user.email <sawepeter6@gmail.com>
2018-05-20 17:04:31 +0300

    fixed a bug 🎨

--- app/src/main/java/com/example/sawepeter/eatout/SignUp.java
+++ app/src/main/java/com/example/sawepeter/eatout/SignUp.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignUp extends AppCompatActivity {
@@ -37,7 +40,17 @@ public class SignUp extends AppCompatActivity {
                 mDialog.setMessage("verifying and authenticating....");
                 mDialog.show();
 
-                
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //check if already user phone is registered
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 
 
             }
