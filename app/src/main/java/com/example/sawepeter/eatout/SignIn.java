e101b895ca5a14b4b550d3b5cc36963eda5a127c
user.email <sawepeter6@gmail.com>
2018-05-20 10:25:20 +0300

    sign.class

--- app/src/main/java/com/example/sawepeter/eatout/SignIn.java
+++ app/src/main/java/com/example/sawepeter/eatout/SignIn.java
@@ -2,12 +2,23 @@ package com.example.sawepeter.eatout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignIn extends AppCompatActivity {
+
+    EditText edtphone,edtpassword;
+    Button btnsignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        edtpassword = (MaterialEditText)findViewById(R.id.edtnpass);
+        edtphone = (MaterialEditText)findViewById(R.id.edtphone);
+        btnsignin = (Button)findViewById(R.id.btnsignin);
     }
 }
