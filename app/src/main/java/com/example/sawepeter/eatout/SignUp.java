e877fcfa7e4638ae943fb3f7b77ccbc662e4d614
user.email <sawepeter6@gmail.com>
2018-05-20 16:56:40 +0300

    �firebase intialization

--- app/src/main/java/com/example/sawepeter/eatout/SignUp.java
+++ app/src/main/java/com/example/sawepeter/eatout/SignUp.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignUp extends AppCompatActivity {
@@ -15,5 +17,15 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        edtname = (MaterialEditText)findViewById(R.id.edtname);
+        edtpassword = (MaterialEditText)findViewById(R.id.edtnpass);
+        edtphone = (MaterialEditText)findViewById(R.id.edtphone);
+
+        btnsignup = (Button)findViewById(R.id.btnsignup);
+
+        //intialize firebase database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("user");
     }
 }
