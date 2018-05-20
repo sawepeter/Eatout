24f358714a6f3578cf12575431c95d6302c0a121
user.email <sawepeter6@gmail.com>
2018-05-20 09:25:23 +0300

    fbutton properties

--- app/src/main/java/com/example/sawepeter/eatout/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/eatout/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.sawepeter.eatout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
