3d5c662fbffe281d4ab21c8dfd60d5375c8701ea
user.email <sawepeter6@gmail.com>
2018-05-20 09:49:33 +0300

    onclicklistener

--- app/src/main/java/com/example/sawepeter/eatout/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/eatout/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sawepeter.eatout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnsignin);
+        btnSignUp = (Button)findViewById(R.id.btnsignup);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
