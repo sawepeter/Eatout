bc2169e9677b0eda7a4e79f63d3bc02e8fa0a7b1
user.email <sawepeter6@gmail.com>
2018-05-20 10:00:27 +0300

    activity

--- app/src/main/java/com/example/sawepeter/eatout/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/eatout/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
     }
